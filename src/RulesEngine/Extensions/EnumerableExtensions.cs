866b121d4eb0c4e198746f3d0c3738665e05f2fc
Aaron Sulwer <asulwer@gmail.com>
2026-05-28 13:06:28 -0700

    formatting

--- src/RulesEngine/Extensions/EnumerableExtensions.cs
+++ src/RulesEngine/Extensions/EnumerableExtensions.cs
@@ -16,8 +16,6 @@ namespace RulesEngine.Extensions
 
         public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,Func<TSource, TKey> keySelector)
         {
-            
-
             var seenKeys = new HashSet<TKey>();
 
             foreach (var element in source)
